fccf3a5f543bd6b34197fc659b537e9940c3e3c8
SikHye1977 <sikhye1977@naver.com>
2025-01-01 20:48:54 +0900

    Solve 1551

---
+++ SolveAc/250101/1551.py
@@ -0,0 +1,17 @@
+N, K = map(int,input().split())
+
+arr = list(map(int,input().split(',')))
+ans = [0] * len(arr)
+
+if(K == 0):
+  print(*arr, sep=',')
+else:
+  for i in range(K):
+    for i in range(len(ans) - 1):
+      ans[i] = arr[i+1]-arr[i]
+    ans.pop(len(ans)-1)
+    arr = ans
+  print(*ans, sep=',')
+  
+# 문제 조건을 제대로 읽어보자
+# 출력값 형식을 제대로 지정하자
