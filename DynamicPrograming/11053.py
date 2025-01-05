f910551305e6a3b588de8c1ca5b5968e981bb4c6
SikHye1977 <sikhye1977@naver.com>
2025-01-05 15:54:50 +0900

    Solve 11055

--- DynamicPrograming/11053.py
+++ DynamicPrograming/11053.py
@@ -1,3 +1,5 @@
+# 가장 긴 부분수열 / Silver 2 / 40m
+
 N = int(input())
 
 arr = list(map(int,input().split()))
@@ -15,3 +17,5 @@ print(max(dp))
 # 두번째 반복문이 그 이전의 수들
 # 이전의 수(j)가 이후의 수(i)보다 작으면 현재 dp[i]의 값과 dp[j]+1을 비교해 큰 값을 dp에 배치
 # 이후 dp 배열의 최댓값 출력
+
+# https://it-adventures.tistory.com/109
