e374dfa4639fcd1754293b271d826973e7461043
SikHye1977 <sikhye1977@naver.com>
2025-02-17 14:38:23 +0900

    solve 16395 for solve.ac marathon

--- Sort/1181.py
+++ Sort/1181.py
@@ -15,7 +15,7 @@ arr.sort(key = len)
 for i in range(len(arr)):
   print(arr[i])
   
-# 파이썬 내장 함수를 잘 활요해야함
+# 파이썬 내장 함수를 잘 활용해야함
 # set으로 중복을 없앰
 # arr.sort()로 알파벳순으로 먼저 정렬
 # arr.sort(key = len)으로 길이순으로 정렬
